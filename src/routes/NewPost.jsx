443415f0cffd5c20403d9e4d36598ea70bb2a4cc
Virgílio César <enjels@outlook.com>
2024-04-03 16:08:57 -0300

    adicionado css para componente NewPost, criado a estrutura para criar novo post com campos form e input textarea

--- src/routes/NewPost.jsx
+++ src/routes/NewPost.jsx
@@ -1,8 +1,29 @@
-
 const NewPost = () => {
   return (
-    <div>NewPost</div>
-  )
-}
+    <div className="new-post">
+      <h2>Inserir novo Post</h2>
+      <form>
+        <div className="form-control">
+          <label htmlFor="title">Título:</label>
+          <input
+            type="text"
+            name="title"
+            placeholder="Digite o título"
+            id="title"
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="body">Conteudo:</label>
+          <textarea
+            name="body"
+            id="body"
+            placeholder="Digite o conteúdo"
+          ></textarea>
+        </div>
+        <input type="submit" value="Criar Post" className="btn"/>
+      </form>
+    </div>
+  );
+};
 
-export default NewPost
+export default NewPost;
